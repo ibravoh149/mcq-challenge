54b66e059517cae44579737182652836666e117a
ibravoh149 <ibravoh149@gmail.com>
2020-07-11 01:18:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import QuestionsHome from "./Pages/QuestionsHome/QuestionsHome";
+
+function App() {
+  return (
+    <div className="App">
+      <QuestionsHome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/QuestionsHome/QuestionsHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionsHome = () => {
+  return <div>Home</div>;
+};
+
+export default QuestionsHome;
